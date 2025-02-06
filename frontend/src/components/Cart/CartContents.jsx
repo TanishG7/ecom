b8665924d6afbe0c73c721a5a6351ea2d2367c39
Tanish Gupta <tanishguptawork@gmail.com>
2025-02-06 20:34:47 +0530

    cart contents

--- frontend/src/components/Cart/CartContents.jsx
+++ frontend/src/components/Cart/CartContents.jsx
@@ -1,15 +1,8 @@
+import { HiMinusSm, HiPlusSm } from 'react-icons/hi'
+
 const CartContents = () => {
 
   const cartProducts = [
-    {
-        productId: 1,
-        name: "T-Shirt",
-        size: "M",
-        color: "Black",
-        quantity: 1,
-        price: 100,
-        image: "https://picsum.photos/200?random=1"
-    },
     {
         productId: 1,
         name: "T-Shirt",
@@ -28,10 +21,35 @@ const CartContents = () => {
         price: 150,
         image: "https://picsum.photos/200?random=2"
     },
-  ]
+  ];
 
   return (
-    <div>CartContents</div>
+    <div>
+      {cartProducts.map((product,index) => (
+        <div 
+          key={index} 
+          className='flex items-center justify-between py-4 border-b'>
+            <div className="flex items-start">
+              <img src={product.image} alt={product.name} className="w-20 h-24 object-cover mr-4 rounded" />
+              <div className="">
+                <h3>{product.name}</h3>
+                <p className="text-sm text-gray-500">
+                  size: {product.size} | color: {product.color}
+                </p>
+                <div className="flex items-center mt-2">
+                  <button className="border rounded px-2 py-1 text-xl font-medium">
+                    <HiMinusSm className="h-6 w-6" />
+                  </button>
+                  <span className="mx-4">{product.quantity}</span>
+                  <button className="border rounded px-2 py-1 text-xl font-medium">
+                    <HiPlusSm className="h-6 w-6" />
+                  </button> 
+                </div>
+              </div>
+            </div>
+        </div>
+      ))}
+    </div>
   )
 }
 
