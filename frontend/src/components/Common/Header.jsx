cf06d87255bf5a4178abe33af074ef6b10f268e3
Tanish Gupta <tanishguptawork@gmail.com>
2025-02-06 12:48:46 +0530

    first files

---
+++ frontend/src/components/Common/Header.jsx
@@ -0,0 +1,13 @@
+import TopBar from '../Layout/TopBar.jsx'
+import Navbar from '../Common/Navbar.jsx'
+
+const Header = () => {
+  return (
+    <header>
+    <TopBar/>
+    <Navbar/>
+    </header>
+  )
+}
+
+export default Header
---
+++ frontend/src/components/Layout/UserLayout.jsx
@@ -0,0 +1,11 @@
+import Header from '../Common/Header.jsx'
+
+const UserLayout = () => {
+  return (
+    <>
+      <Header />
+    </>
+  )
+}
+
+export default UserLayout
