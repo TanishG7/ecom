bce8d0819ec5e799a315cf977d69c3df7b1d538b
Tanish Gupta <tanishguptawork@gmail.com>
2025-02-09 17:47:55 +0530

    add routing for login, register, profile, and collection pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,23 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import UserLayout from './components/Layout/UserLayout'
 import Home from './pages/Home'
+import Login from './pages/Login'
+import { Toaster } from 'sonner'
+import Register from './pages/Register'
+import Profile from './pages/Profile'
+import Collection from './pages/Collection'
 
 const App = () => {
   return (
     <BrowserRouter>
+    <Toaster position="top-right"/>
     <Routes>
       <Route path="/" element={<UserLayout/>}>
         <Route index element={<Home/>}/>  
+        <Route path='login' element={<Login/>}/>
+        <Route path='register' element={<Register/>}/>
+        <Route path='profile' element={<Profile/>}/>
+        <Route path='collections/:collection' element={<Collection/>}/>
       </Route>
     </Routes>
     </BrowserRouter>
