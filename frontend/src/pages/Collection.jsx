bfd17fd5036671a10df75c9806ed5ca5e52036cd
Tanish Gupta <tanishguptawork@gmail.com>
2025-02-12 18:11:00 +0530

    add Collection component to display product collections with filtering and sorting options

---
+++ frontend/src/pages/Collection.jsx
@@ -0,0 +1,108 @@
+import { useState, useEffect, useRef } from "react"
+import FilterSidebar from "../components/Products/FilterSidebar";
+import { FaFilter } from "react-icons/fa"
+import SortOptions from "../components/Products/SortOptions";
+import ProductGrid from "../components/Products/ProductGrid";
+
+const Collection = () => {
+
+    const [products, setProducts] = useState([])
+    const sidebarRef = useRef(null)
+    const [isSidebarOpen, setIsSidebarOpen] = useState(false)
+
+    const toggleSidebar = () => {
+        setIsSidebarOpen(!isSidebarOpen)
+    }
+
+    const handleClickOutside = (e) => {
+        if (sidebarRef.current && !sidebarRef.current.contains(e.target)) {
+            setIsSidebarOpen(false)
+        }
+    }
+
+    useEffect(() => {
+        document.addEventListener("mousedown", handleClickOutside);
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside);
+        };
+    }, []); 
+    
+
+    useEffect(() => {
+        setTimeout(() => {
+            const fetchedProducts = [
+                {
+                    _id: "1",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=3"}],
+                },
+                {
+                    _id: "2",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=4"}],
+                },
+                {
+                    _id: "3",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=5"}],
+                },
+                {
+                    _id: "4",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=6"}],
+                },
+                {
+                    _id: "5",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=7"}],
+                },
+                {
+                    _id: "6",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=8"}],
+                },
+                {
+                    _id: "7",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=9"}],
+                },
+                {
+                    _id: "8",
+                    name: "Product 1",
+                    price: 120,
+                    images: [{url: "https://picsum.photos/500/500?random=10"}],
+                },
+                
+            ];
+            setProducts(fetchedProducts);
+        }, 1000)
+    }, []);
+
+  return (
+    <div className="flex flex-col lg:flex-row">
+        <button onClick={toggleSidebar} className="lg:hidden p-2 border flex justify-center items-center">
+            <FaFilter className="mr-2"/>Filters
+        </button>
+
+        <div 
+        ref={sidebarRef} 
+        className={`${isSidebarOpen ? "translate-x-0" : "-translate-x-full"} fixed inset-y-0 z-50 left-0 w-64 bg-white overflow-y-auto transition-transform duration-300 lg:static lg:translate-x-0`}>
+            <FilterSidebar/>
+        </div>
+        <div className="flex-grow p-4">
+            <h2 className="text-2xl mb-4 uppercase">All Collections</h2>
+            <SortOptions/>
+            <ProductGrid products={products}/>
+        </div>
+    </div>
+  )
+}
+
+export default Collection
