fdfd69e93149e6ddae1d5b67561532bd80c7e979
Tanish Gupta <tanishguptawork@gmail.com>
2025-02-14 22:36:08 +0530

    refactor SortOptions component to remove unused select element

--- frontend/src/components/Products/SortOptions.jsx
+++ frontend/src/components/Products/SortOptions.jsx
@@ -2,11 +2,7 @@
 const SortOptions = () => {
   return (
     <div>
-      <select>
-        <option value="default">Default</option>
-        <option value="price-asc">Price: Low to High</option>
-        <option value="price-desc">Price: High to Low</option>
-      </select>
+      SortOptions
     </div>
   )
 }
